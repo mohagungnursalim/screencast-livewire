714b6e9dfe6e15357a0000e8d8189396cb756783
mohagungnursalim <mohagungnursalim@gmail.com>
2024-04-13 09:41:59 +0800

    feat:setting template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/dashboard', DashboardController::class);
