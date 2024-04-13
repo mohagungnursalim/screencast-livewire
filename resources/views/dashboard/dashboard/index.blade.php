655c5532bcef3256c0482bd2b04c241481b720b9
mohagungnursalim <mohagungnursalim@gmail.com>
2024-04-13 13:27:31 +0800

    feat:setting template dashboard

--- resources/views/dashboard/dashboard/index.blade.php
+++ resources/views/dashboard/dashboard/index.blade.php
@@ -1,1 +1,14 @@
+@extends('dashboard.layouts.main')
+@section('judul-halaman')
+    Dashboard |
+@endsection
 
+@section('konten')
+
+<div class="container">
+    <h3>Ini halaman Dashboard</h3>
+</div>
+
+
+
+@endsection
